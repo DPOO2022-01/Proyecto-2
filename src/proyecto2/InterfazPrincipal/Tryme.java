86492690f0900e5a6b5b6f16659f9236dc42dae2
CristianCaro915 <c.caro@uniandes.edu.co>
2022-04-26 08:20:33 -0700

    aa

--- src/proyecto2/InterfazPrincipal/Tryme.java
+++ src/proyecto2/InterfazPrincipal/Tryme.java
@@ -47,6 +47,32 @@ public class Tryme {
 	        txtEjemplo2.setEditable( false );
 	        add( txtEjemplo2 );
 		    //elemento6
+		    //elemento1
+		    JTextField txtEjemplo= new JTextField( " Proyecto Actual " );
+	        txtEjemplo.setEditable( false );
+	        add( txtEjemplo );
+		    //elemento2
+	        JTextField txtEjemplo11= new JTextField( " nombre del proyecto " );
+	        txtEjemplo11.setEditable( false );
+	        add( txtEjemplo11 );
+		    //elemento3
+	        JTextField txtEjemplo1= new JTextField( " Participantes " );
+	        txtEjemplo1.setEditable( false );
+	        add( txtEjemplo1 );
+		    //elemento4
+	        JTextField txtEjemplo12= new JTextField( " item1 de la lista " );
+	        txtEjemplo12.setEditable( false );
+	        JTextField txtEjemplo13= new JTextField( " item2 de la lista " );
+	        txtEjemplo13.setEditable( false );
+	        
+	        JList<JTextField> lstEjemplo= new JList<JTextField>();
+	        lstEjemplo.add(txtEjemplo12);
+	        lstEjemplo.add(txtEjemplo13);
+		    //elemento5
+	        JTextField txtEjemplo2= new JTextField( " Actividades " );
+	        txtEjemplo2.setEditable( false );
+	        add( txtEjemplo2 );
+		    //elemento6
 	        
 		}
 	}
