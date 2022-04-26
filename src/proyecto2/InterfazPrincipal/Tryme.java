f54f3fc618f4da7f95a811e24323c60343573141
CristianCaro915 <c.caro@uniandes.edu.co>
2022-04-26 08:11:03 -0700

    AA

--- src/proyecto2/InterfazPrincipal/Tryme.java
+++ src/proyecto2/InterfazPrincipal/Tryme.java
@@ -3,4 +3,81 @@ package proyecto2.InterfazPrincipal;
 public class Tryme {
 	
 	public Tryme() {}
+	/*
+	package uniandes.dpoo.taller4.interfaz;
+
+	import java.awt.BorderLayout;
+	import java.awt.GridLayout;
+
+	import javax.swing.JList;
+	import javax.swing.JPanel;
+	import javax.swing.JTextField;
+	import javax.swing.border.TitledBorder;
+
+	public class PanelCentro extends JPanel{
+		public PanelCentro() {
+			setLayout( new BorderLayout( ) );
+		    setBorder( new TitledBorder( " Mostrador de información " ) );
+		    
+		    setLayout( new GridLayout( 6 , 1 ) );
+		    
+		    //elemento1
+		    JTextField txtEjemplo= new JTextField( " Proyecto Actual " );
+	        txtEjemplo.setEditable( false );
+	        add( txtEjemplo );
+		    //elemento2
+	        JTextField txtEjemplo11= new JTextField( " nombre del proyecto " );
+	        txtEjemplo11.setEditable( false );
+	        add( txtEjemplo11 );
+		    //elemento3
+	        JTextField txtEjemplo1= new JTextField( " Participantes " );
+	        txtEjemplo1.setEditable( false );
+	        add( txtEjemplo1 );
+		    //elemento4
+	        JTextField txtEjemplo12= new JTextField( " item1 de la lista " );
+	        txtEjemplo12.setEditable( false );
+	        JTextField txtEjemplo13= new JTextField( " item2 de la lista " );
+	        txtEjemplo13.setEditable( false );
+	        
+	        JList<JTextField> lstEjemplo= new JList<JTextField>();
+	        lstEjemplo.add(txtEjemplo12);
+	        lstEjemplo.add(txtEjemplo13);
+		    //elemento5
+	        JTextField txtEjemplo2= new JTextField( " Actividades " );
+	        txtEjemplo2.setEditable( false );
+	        add( txtEjemplo2 );
+		    //elemento6
+	        
+	        // PANEL SHOW ACTIVIDADES
+		    
+	        JPanel panelShowActividades= new JPanel();
+		    add(panelShowActividades,BorderLayout.SOUTH);
+		    
+		    panelShowActividades.setLayout(new GridLayout(5,4));
+		    panelShowActividades.setBorder( new TitledBorder( "Actividades Disponibles" ));
+		    //elemento de tabla 1
+		    JTextField txtEjemplo111= new JTextField( " Nombre " );
+	        txtEjemplo111.setEditable( true );
+	        panelShowActividades.add( txtEjemplo111 );
+			//elemento de tabla 2
+	        JTextField txtEjemplo112= new JTextField( " Tipo " );
+	        txtEjemplo112.setEditable( true );
+	        panelShowActividades.add( txtEjemplo112 );
+			//elemento de tabla 3
+	        JTextField txtEjemplo113= new JTextField( " Descripción " );
+	        txtEjemplo113.setEditable( true );
+	        add( txtEjemplo113 );
+			//elemento de tabla 4
+	        JTextField txtEjemplo114= new JTextField( " Participante Asignado " );
+	        txtEjemplo114.setEditable( true );
+	        add( txtEjemplo114 );
+		    //elemento de tabla 5
+	        JTextField txtEjemplo115= new JTextField( " null " );
+	        txtEjemplo115.setEditable( true );
+	        add( txtEjemplo115 );
+		    
+		    
+		}
+	}
+	*/
 }
