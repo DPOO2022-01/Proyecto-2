49b84f1bed0bf8e3f8ae700802328d6799e897d6
CristianCaro915 <69738827+CristianCaro915@users.noreply.github.com>
2022-04-26 08:19:43 -0700

    Add files via upload

---
+++ Proyecto2_DPOO_2022/src/proyecto2/InterfazPrincipal/Tryme.java
@@ -0,0 +1,54 @@
+package proyecto2.InterfazPrincipal;
+
+public class Tryme {
+	
+	public Tryme() {}
+	/*
+	package uniandes.dpoo.taller4.interfaz;
+
+	import java.awt.BorderLayout;
+	import java.awt.GridLayout;
+
+	import javax.swing.JList;
+	import javax.swing.JPanel;
+	import javax.swing.JTextField;
+	import javax.swing.border.TitledBorder;
+
+	public class PanelCentro extends JPanel{
+		public PanelCentro() {
+			setLayout( new BorderLayout( ) );
+		    setBorder( new TitledBorder( " Mostrador de información " ) );
+		    
+		    setLayout( new GridLayout( 6 , 1 ) );
+		    
+		    //elemento1
+		    JTextField txtEjemplo= new JTextField( " Proyecto Actual " );
+	        txtEjemplo.setEditable( false );
+	        add( txtEjemplo );
+		    //elemento2
+	        JTextField txtEjemplo11= new JTextField( " nombre del proyecto " );
+	        txtEjemplo11.setEditable( false );
+	        add( txtEjemplo11 );
+		    //elemento3
+	        JTextField txtEjemplo1= new JTextField( " Participantes " );
+	        txtEjemplo1.setEditable( false );
+	        add( txtEjemplo1 );
+		    //elemento4
+	        JTextField txtEjemplo12= new JTextField( " item1 de la lista " );
+	        txtEjemplo12.setEditable( false );
+	        JTextField txtEjemplo13= new JTextField( " item2 de la lista " );
+	        txtEjemplo13.setEditable( false );
+	        
+	        JList<JTextField> lstEjemplo= new JList<JTextField>();
+	        lstEjemplo.add(txtEjemplo12);
+	        lstEjemplo.add(txtEjemplo13);
+		    //elemento5
+	        JTextField txtEjemplo2= new JTextField( " Actividades " );
+	        txtEjemplo2.setEditable( false );
+	        add( txtEjemplo2 );
+		    //elemento6
+	        
+		}
+	}
+	*/
+}
